7c48454b5a725fa05560b4902a36e05d080e54f0
juancruz <juancruz@juan>
2017-02-01 00:02:47 -0300

    comienzo de definicion del cliente

--- puntoventa/index.js
+++ puntoventa/index.js
@@ -1,46 +1,87 @@
-/* global require, c, s */
+/*
+ * Module dependencies
+ */
+/* global __dirname, ciudades */
 
 var express = require('express');
-var argv = require('yargs')
-			.usage('Usar: $0 -p [num]')
-			.demand(['p'])
-			.argv;
+var stylus = require('stylus');
+var nib = require('nib');
+
 var app = express();
-var unirest = require('unirest');
-var extend = require('extend');
-//var io = require('socket.io-client');
+function compile(str, path) {
+  return stylus(str)
+    .set('filename', path)
+    .use(nib());
+}
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+//app.use(express.logger('dev'));
+logger = require('morgan');
+app.use(logger('dev'));
+app.use(stylus.middleware(
+  { src: __dirname + '/public'
+  , compile: compile
+  }
+));
 
+app.use(express.static(__dirname + '/public'));
 
-app.listen(argv.p, function () {
-  	console.log('Punto de venta escuchando en puerto ' + argv.p);
+var empresas = ["http://localhost:8080","http://localhost:8081","http://localhost:8082"];
+
+//ciudades = ["Río Cuarto", "Córdoba", "Buenos Aires", "Rosario", "Rio de Janeiro", "Brasilia","New York", "Los Angeles"];
+var ciudades = function(){
+    for (var i=0; i < empresas.length; i++) {
+        var serverUrl = empresas[i];
+
+        var xhttp = new XMLHttpRequest();
+        xhttp.serverUrl = serverUrl;
+        xhttp.onreadystatechange = function() {
+          if (this.readyState == 4 && this.status == 200) {
+            var jsonTramos = JSON.parse(this.responseText);
+            agregarCiudad(jsonTramos);
+          }
+        };
+        xhttp.open("GET", serverUrl+"/list", true);
+        xhttp.send();
+    }
+};
+  
+app.get('/', function (req, res) {
+  res.render('index',
+  { title : 'Home', ciudades: ciudades}
+  );
 });
+app.listen(3000);
 
-var companies = {
-				"empresa1" : {
-								"host": "localhost",
-								"port": "3000", 
-								"stretches": "/stretches"
-							 },
-				"empresa2" : {
-								"host": "localhost",
-								"port": "3001", 
-								"stretches": "/stretches"
-							 }
+app.get('/add', function (req, res) {
+    var serverUrl = "http://localhost:8080";
 
-			};
-
-function connectSocket(url){
-	var socket = require('socket.io-client')(url, {forceNew: true});
-	socket.on('connect', function () { console.log("Empresa conectada: " + url ); });
-	socket.on('disconnect', function () { console.log("Empresa desconectada: " + url); });
+    var xhttp = new XMLHttpRequest();
+    xhttp.serverUrl = serverUrl;
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        var jsonTramos = JSON.parse(this.responseText);
+        agregarCiudad(jsonTramos);
+      }
+    };
+    xhttp.open("GET", serverUrl+"/list", true);
+    xhttp.send();
+});
+function agregarCiudad(tramos){
+    
+    console.log(tramos[0].cOrigen);
 }
 
-for (c in companies) {
-  var url = 'http://' + companies[c].host + ':' + companies[c].port + '/';
-  connectSocket(url);
+function contiene(ciudad){
+    var contiene = false;
+    for (var i = 0; i < ciudades.length; i++) {
+        if (ciudades[i]===ciudad) {
+            contiene = true;
+            break;
+        }
+    }
+    return contiene;
 }
-
-
 // console.log(companies.empresa1.host);
 // console.log(companies.empresa1.port);
 // console.log(companies.empresa1.stretches);
@@ -57,6 +98,7 @@ for (c in companies) {
 
 var graph = function(){
 	var result = {};
+        var s;
 	for (s in stretches){
 		var arr = s.split('-');
 		if (!result[arr[0]]){
@@ -69,11 +111,11 @@ var graph = function(){
 		
 	}
 	return result;
-}
-
+};
 
 var stretches = function(callback){
 	var result = {};
+        var c;
 	for (c in companies){
 		var url = 'http://' + companies[c].host + ':' + companies[c].port + companies[c].stretches;
 		unirest.get(url).end(function(res) {
@@ -89,13 +131,7 @@ var stretches = function(callback){
 
 		
 	}
-}
-
-
-	  	
-
-
-
+};
 
 // var graph = {'A': ['B', 'C'],
 //              'B': ['C', 'D'],
