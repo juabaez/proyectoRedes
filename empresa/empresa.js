f8abb46581caa8e3c7edf9eb8b01c6c45dcf7b50
juancruz <juancruz@juan>
2017-02-12 17:54:31 -0300

    modificacion en la reserva y actualizar la cantidad de asientos y reservas para cada tramo todavia no funciona la reserva

--- empresa/empresa.js
+++ empresa/empresa.js
@@ -4,6 +4,8 @@ var express = require('express');
 var app = express();
 var fs = require("fs");
 
+var path = require('path');
+
 var bodyParser = require('body-parser');
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 
@@ -14,11 +16,11 @@ app.use(function(req, res, next) {
 });
 
 //Reservas 
-var reserves = [];
+var reservas = [];
 var ires = 3;
 //Reservas base
-reserves.push({idReserv: 1, name: "Juan Baez",date:"2017-01-01",idTramo:1});
-reserves.push({idReserv: 2, name: "Juan Baez",date:"2017-01-01",idTramo:2});
+reservas.push({idReserv: 1, name: "Juan Baez",date:"2017-01-01",idTramo:1});
+reservas.push({idReserv: 2, name: "Juan Baez",date:"2017-01-01",idTramo:2});
 
 //Retorna si hay disponibles cierta cantidad de asientos en un tramo
 var disponible = function(cant, reservados){
@@ -30,60 +32,84 @@ app.get('/list', function (req, res) {
     fs.readFile( __dirname + "/datos/" + "tramos.json", 'utf8', function (err, data) {
        res.end(data);
     });
+    
 });
 
 app.post('/reservar', function (req, res) {
-   // Get the travel, and make the reservation if it can be done.
     console.log(req.params);
     console.log(req.body);
     cancelarReserva();
-    var travelId = req.body.travelId;
+    var tramoId = req.body.tramoId;
     var tramos;
-    fs.readFile( __dirname + "/datos/" + "tramos.json", 'utf8', function (err, data) {
-        tramos = JSON.parse( data );
+    //Normalizing the path for windows environment
+    var normalPath = path.normalize( __dirname + "/datos/" + 'tramos.json');
+    fs.readFile( normalPath, 'utf8', function (err, data) {
+        //tramos = JSON.parse(data);
+        tramos = data;
     }); 
-    if (disponible(tramos[travelId-1].places,tramos[travelId-1].reserved)) {
-        reserves.push({idReserv: ires, name: "Juan Baez",date:new Date().toLocaleDateString(),idTramo:travelId-1});
-        console.log("Nueva Reserva: " +
-                {idReserv: ires, name: "Juan Baez",date:new Date().toLocaleDateString(),idTramo:travelId-1});
+    console.log(tramos);
+    console.log("cantidad "+tramos[tramoId].cantidad +" "+tramos[tramoId].reservado);
+    console.log(disponible(tramos[tramoId].cantidad,tramos[tramoId].reservado));
+    /*if (disponible(tramos[tramoId-1].places,tramos[tramoId-1].reserved)) {
+        reservas.push({idReserv: ires, name: "Juan Baez",date:new Date().toLocaleDateString(),idTramo:tramoId-1});
+        console.log(reservas);
+        actualizarReservas(tramos,tramoId-1);
         ires++;
         res.end("OK");
     } else {
         res.end("FALLO");
-    }
+    }*/
 });
 
 function cancelarReserva(){
-    for (var i = reserves.length -1; i >=0 ; i--) {
-        //console.log("Parse cada reserva: "+reserves[i]);
+    for (var i = reservas.length -1; i >=0 ; i--) {
+        //console.log("Parse cada reserva: "+reservas[i]);
         var diff = server1;
-        var dateReserva = new Date(reserves[i].date).getTime();
+        var dateReserva = new Date(reservas[i].date).getTime();
         var dateNow = new Date().getTime();
         var diffDate = (dateNow-dateReserva)/(1000*60*60*24);
         diffDate = diffDate.toPrecision(2)-1;
         console.log("Diff dias para cancelar: "+diff);
         console.log("Diff entre hoy y la reserva: "+diffDate);
         if (diffDate>diff) {
-            reserves.pop();
-            //reserves.remove(i);
+            reservas.pop();
         }
-        if(reserves.length===0){
+        if(reservas.length===0){
             ires=1;
         }
     }
+    console.log("Reservas luego de cancelarlas automaticamente");
+    console.log(reservas);
 }
+
+function actualizarReservas(tramos,idTramo){
+    for (var i = 0; i < tramos.length; i++) {
+        if (tramos[i].id == idTramo) {
+            tramos[i].cantidad = tramos[i].cantidad - 1;
+            tramos[i].reservado = tramos[i].reservado + 1;
+            break;
+        }
+    }
+    fs.writeFile(__dirname + "/datos/" + "tramos.json",JSON.stringify(tramos),function(error){
+        if (error)
+            console.log(error);
+        else
+            console.log('Se actualizo la cantidad y las reservas con exito');
+    });
+}
+
 var server = app.listen(8080, function () {
 
-  var host = server.address().address;
-  var port = server.address().port;
+  //var host = server.address().address;
+  //var port = server.address().port;
   console.log("Reservas al comenzar");
-  console.log(reserves);
+  console.log(reservas);
   //console.log("Despues de cancelar las reservas");
-  //console.log(reserves);
-  //reserves.push({idReserv: ires, name: "Juan Baez",date:"2017-01-01",idTramo:3});
+  //console.log(reservas);
+  //reservas.push({idReserv: ires, name: "Juan Baez",date:"2017-01-01",idTramo:3});
   //console.log("NEW");
-  //console.log(reserves);
-  console.log("Listen http://localhost:%s", port);
+  //console.log(reservas);
+  console.log("Listen http://localhost:%s", server.address().port);
 
 });
 
