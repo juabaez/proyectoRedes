135184958b9f857ff578051b7d775feddc3e6aa0
juancruz <juancruz@juan>
2017-02-12 17:53:07 -0300

    agregar los tramos al grafo y buscar los tramos, todavia no funcional a reserva de pasajes

--- puntoventa/public/js/travels-functions.js
+++ puntoventa/public/js/travels-functions.js
@@ -1,34 +1,27 @@
-var enterpriseServers = ["http://localhost:8080","http://localhost:8081","http://localhost:8082"];
+var empresas = ["http://localhost:8080","http://localhost:8081","http://localhost:8082"];
 
-var travels = new graphlib.Graph(); // creates a graph
+var grafoTramos = new graphlib.Graph(); //crea un graph
 var paths = [];
 
-// Timestamp for event synchronization
-var time_stamp=0;
 
-/**
- * searchTravels(): this function call to all the enterprise servers seaching for travels. And then each 
- * time that get the response of a server it tries to calculate if there is a possible way from the origin 
- * to the destiny.
- */
-function searchTravels() {
-  var originSelect = document.getElementById("origin");
-  var destinySelect = document.getElementById("destiny");
+function buscar() {
+  var paramOrigen = document.getElementById("origin");
+  var paramDestino = document.getElementById("destiny");
   
-  var originCity = originSelect.options[originSelect.value].text;
-  var destinyCity = destinySelect.options[destinySelect.value].text;
+  var cOrigen = paramOrigen.options[paramOrigen.value].text;
+  var cDestino = paramDestino.options[paramDestino.value].text;
   
-  console.log('Origen: '+originCity+' - Destino: '+destinyCity);
+  console.log(cOrigen+' - '+cDestino);
 
-  for (var i=0; i < enterpriseServers.length; i++) {
-    var serverUrl = enterpriseServers[i];
+  for (var i=0; i < empresas.length; i++) {
+    var serverUrl = empresas[i];
 
     var xhttp = new XMLHttpRequest();
     xhttp.serverUrl = serverUrl;
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
-        var jsonTravels = JSON.parse(this.responseText);
-        findTravels(originCity,destinyCity,jsonTravels,this.serverUrl);
+        var tramos = JSON.parse(this.responseText);
+        findTravels(cOrigen,cDestino,tramos,this.serverUrl);
       }
     };
     xhttp.open("GET", serverUrl+"/list", true);
@@ -37,50 +30,106 @@ function searchTravels() {
 }
 
 /**
- * findTravels(originCity,destinyCity,jsonTravels,serverUrl): this function calculates all the possible ways
- * to go from the originCity to the destinyCity according the current travels information and the given
+ * findTravels(cOrigen,cDestino,jsonTravels,serverUrl): this function calculates all the possible ways
+ * to go from the cOrigen to the cDestino according the current travels information and the given
  * in jsonTravels.
  */
-function findTravels(originCity,destinyCity,jsonTravels,serverUrl) {
-  saveTravels(jsonTravels,serverUrl);
+function findTravels(cOrigen,cDestino,tramos,serverUrl) {
+  addGrafo(tramos,serverUrl);
   paths = [];
-  findAllPaths(travels,originCity,destinyCity,[originCity]);
-  fillTravelsTable(paths);
+  findAllPaths(grafoTramos,cOrigen,cDestino,[cOrigen]);
+  addTramoTable(paths);
 }
 
 /**
- * saveTravels(originCity,destinyCity,jsonTravels): this function saves the given travels to the travels graph.
+ * saveTravels(cOrigen,cDestino,tramos): this function saves the given travels to the travels graph.
  */
-function saveTravels(jsonTravels,serverUrl) {
+function addGrafo(tramos,serverUrl) {
   var i;
-  for(i = 0; i < jsonTravels.length; i++) {
-    var travel = jsonTravels[i];
-    travel.providerUrl = serverUrl; 
-    if (!travels.hasNode(travel.originCity))
-      travels.setNode(travel.originCity);
-    if (!travels.hasNode(travel.destinyCity))
-      travels.setNode(travel.destinyCity);
-    travels.setEdge(travel.originCity,travel.destinyCity,travel);
+  for(i = 0; i < tramos.length; i++) {
+    var viaje = tramos[i];
+    viaje.providerUrl = serverUrl; 
+    if (!grafoTramos.hasNode(viaje.cOrigen)){
+      grafoTramos.setNode(viaje.cOrigen);
+    }
+    if (!grafoTramos.hasNode(viaje.cDestino)){
+      grafoTramos.setNode(viaje.cDestino);
+    }
+    grafoTramos.setEdge(viaje.cOrigen,viaje.cDestino,viaje);
   }
 }
 
 /**
  * reserveTravels(path): reserve the travels in the given path.
  */
-function reserveTravels(path){
+function reservar(path){
   for(var i=0; i < path.length-1; i++) {
-    var travelInPath = travels.edge({ v: path[i], w: path[i+1] });
-    
+    var tramo = grafoTramos.edge({ v: path[i], w: path[i+1] });
+    console.log(tramo);
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
-        console.log("RESERVATION RESPONSE");
+        console.log("RESERVA RESPONSE");
         console.log(this.responseText);
       }
     };
-    xhttp.open("POST", travelInPath.providerUrl +"/reservation", true);
+    xhttp.open("POST", tramo.providerUrl +"/reservar", true);
     xhttp.setRequestHeader("Content-Type", "application/json");
-    time_stamp++;
-    xhttp.send(JSON.stringify({travelId:travelInPath.id, time_stamp:time_stamp}));
+    xhttp.send(JSON.stringify({tramoId:tramo.id}));
   }
+}
+
+function addTramoTable(paths){
+    var tableDiv = document.getElementById("table");
+    while(tableDiv.firstChild){
+        tableDiv.removeChild(tableDiv.firstChild);
+    }
+    tableDiv.style.width = "500px";
+
+    for (var i=0; i<paths.length; i++){
+        var pathTr = document.createElement('TR');
+
+        var path = paths[i];
+        var pathTable = document.createElement('TABLE');
+        pathTable.border='1';
+
+        var pathTableCaption = document.createElement('CAPTION');
+        pathTableCaption.appendChild(document.createTextNode("Opción "+(i+1)));
+        pathTable.appendChild(pathTableCaption);
+
+        var pathTableBody = document.createElement('TBODY');
+        pathTable.appendChild(pathTableBody);
+
+        var pathTd = document.createElement('TD');
+        pathTd.width='1000';
+        pathTd.appendChild(pathTable);
+        for (var j=0; j<path.length-1; j++){
+            var travelTr = document.createElement('TR');
+            pathTableBody.appendChild(travelTr);
+            var origenTd = document.createElement('TD');
+            var destinoTd = document.createElement('TD');
+            origenTd.style.paddingTop='10px';
+            origenTd.width='100';
+            origenTd.appendChild(document.createTextNode(path[j]));
+            destinoTd.style.paddingTop='10px';
+            destinoTd.width='100';
+            destinoTd.appendChild(document.createTextNode(path[j+1]));
+            travelTr.appendChild(origenTd);
+            travelTr.appendChild(destinoTd);
+        }
+
+        pathTr.appendChild(pathTd);
+        tableDiv.appendChild(pathTable);
+        var buttonReserva = document.createElement('BUTTON');
+        buttonReserva.appendChild(document.createTextNode('Reservar'));
+        buttonReserva.style.float='right';
+        buttonReserva.travel = path;
+        buttonReserva.onclick = function(){
+            reservar(this.travel);
+        };
+        tableDiv.appendChild(buttonReserva);
+        tableDiv.appendChild(document.createElement('BR'));
+        tableDiv.appendChild(document.createElement('BR'));
+    }
+
 }
